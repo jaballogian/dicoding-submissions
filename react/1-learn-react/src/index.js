528f7bc0b1daa8fd136f5fc6fb4a374bbfe783e7
jaballogian <jaballogianrma77@gmail.com>
2022-12-28 21:59:36 +0700

    add Theme Provider and Styled Engine Provider components

--- react/1-learn-react/src/index.js
+++ react/1-learn-react/src/index.js
@@ -4,10 +4,27 @@ import ReactDOM from 'react-dom/client'
 // COMPONENTS
 import App from './App'
 
+// MUIS
+import { createTheme } from '@mui/material'
+import { 
+  StyledEngineProvider, 
+  ThemeProvider, 
+} from '@mui/material/styles'
+
 // STYLES
 import './index.css'
 
+const customTheme = createTheme({
+  typography: {
+    fontFamily: [ 'Montserrat', 'sans-serif' ].join(','),
+  },
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <App />
+  <ThemeProvider theme={customTheme}>
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
+  </ThemeProvider>
 )
