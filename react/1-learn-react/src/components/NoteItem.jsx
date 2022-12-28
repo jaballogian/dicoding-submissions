8589cb77934babac46b2e839fd061e46e2508a04
jaballogian <jaballogianrma77@gmail.com>
2022-12-28 22:45:30 +0700

    separate Note Item component to its own component

---
+++ react/1-learn-react/src/components/NoteItem.jsx
@@ -0,0 +1,67 @@
+// MUIS
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardHeader from '@mui/material/CardHeader'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+
+// UTILITIES
+import { showFormattedDate } from 'utilities/data'
+
+const NoteItem = (props) => {
+  const { title, createdAt, body } = props
+
+  return (
+    <Grid
+      item
+      xs={6}
+    >
+      <Card sx={(theme) => ({
+        borderRadius: '8px',
+        border: `1px solid ${theme.palette.common.white}`,
+      })}>
+        {/* HEADER */}
+        <CardHeader
+          title={title}
+          subheader={showFormattedDate(createdAt)}
+        />
+
+        {/* CONTENT */}
+        <CardContent>
+          <Typography
+            variant='subtitle1'
+            textAlign='justify'
+            flex='1'
+          >
+            {body}
+          </Typography>
+        </CardContent>
+
+        {/* ACTIONS */}
+        <CardActions>
+          {/* DELETE BUTTON */}
+          <Button
+            color='error'
+            variant='contained'
+            fullWidth
+          >
+            Delete
+          </Button>
+
+          {/* ARCHIVE BUTTON */}
+          <Button
+            color='primary'
+            variant='contained'
+            fullWidth
+          >
+            Archive
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  )
+}
+
+export default NoteItem
--- react/1-learn-react/src/components/NoteList.jsx
+++ react/1-learn-react/src/components/NoteList.jsx
@@ -1,15 +1,10 @@
+// COMPONENTS
+import NoteItem from 'components/NoteItem'
+
 // MUIS
-import Button from '@mui/material/Button'
-import Card from '@mui/material/Card'
-import CardActions from '@mui/material/CardActions'
-import CardContent from '@mui/material/CardContent'
-import CardHeader from '@mui/material/CardHeader'
 import Grid from '@mui/material/Grid'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
-
-// UTILITIES
-import { showFormattedDate } from 'utilities/data'
 
 const NoteList = (props) => {
   const { noteList, title } = props
@@ -38,54 +33,10 @@ const NoteList = (props) => {
           spacing={24}
         >
           {noteList.map(item => (
-            <Grid
+            <NoteItem
               key={item.id}
-              item
-              xs={6}
-            >
-              <Card sx={(theme) => ({
-                borderRadius: '8px',
-                border: `1px solid ${theme.palette.common.white}`,
-              })}>
-                {/* HEADER */}
-                <CardHeader
-                  title={item.title}
-                  subheader={showFormattedDate(item.createdAt)}
-                />
-
-                {/* CONTENT */}
-                <CardContent>
-                  <Typography
-                    variant='subtitle1'
-                    textAlign='justify'
-                    flex='1'
-                  >
-                    {item.body}
-                  </Typography>
-                </CardContent>
-
-                {/* ACTIONS */}
-                <CardActions>
-                  {/* DELETE BUTTON */}
-                  <Button
-                    color='error'
-                    variant='contained'
-                    fullWidth
-                  >
-                    Delete
-                  </Button>
-
-                  {/* ARCHIVE BUTTON */}
-                  <Button
-                    color='primary'
-                    variant='contained'
-                    fullWidth
-                  >
-                    Archive
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
+              {...item}
+            />
           ))}
         </Grid>
       </Stack>
