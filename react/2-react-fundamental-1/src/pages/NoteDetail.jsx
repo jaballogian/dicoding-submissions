d31f01b5505601d6e1b4f120cdc8f2cca86d6767
jaballogian <jaballogianrma77@gmail.com>
2023-01-02 22:56:21 +0700

    simplify the Note Detail page

--- react/2-react-fundamental-1/src/pages/NoteDetail.jsx
+++ react/2-react-fundamental-1/src/pages/NoteDetail.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import PropTypes from 'prop-types'
 import { useParams } from 'react-router-dom'
 
 // MUIS
@@ -8,55 +8,53 @@ import Typography from '@mui/material/Typography'
 // UTILITIES
 import { showFormattedDate } from 'utilities/data'
 
-class NoteDetail extends Component {
-  render () {
-    const { note } = this.props
-
-    return (
-      <Stack padding={40}>
-        {/* TITLE */}
-        <Typography
-          variant='h4'
-          component='h2'
-          fontWeight={600}
-          color='text.primary'
-          marginBottom={12}
-        >
-          {note.title}
-        </Typography>
-
-        {/* CREATED AT */}
-        <Typography
-          variant='body2'
-          color='text.secondary'
-          marginBottom={24}
-        >
-          {showFormattedDate(note.createdAt)}
-        </Typography>
-
-        {/* BODY */}
-        <Typography
-          variant='subtitle1'
-          color='text.primary'
-        >
-          {note.body}
-        </Typography>
-      </Stack>
-    )
-  }  
-}
-
-const NoteDetailWrapper = (props) => {
+const NoteDetail = (props) => {
   const { filteredNoteList } = props
 
   const { id } = useParams()
 
+  const selectedNote = filteredNoteList.find(item => item.id === id || item.id === Number(id))
+
   return (
-    <NoteDetail 
-      id={id}
-      note={filteredNoteList.find(item => item.id === id || item.id === Number(id))}
-    />
+    <Stack padding={40}>
+      {/* TITLE */}
+      <Typography
+        variant='h4'
+        component='h2'
+        fontWeight={600}
+        color='text.primary'
+        marginBottom={12}
+      >
+        {selectedNote.title}
+      </Typography>
+
+      {/* CREATED AT */}
+      <Typography
+        variant='body2'
+        color='text.secondary'
+        marginBottom={24}
+      >
+        {showFormattedDate(selectedNote.createdAt)}
+      </Typography>
+
+      {/* BODY */}
+      <Typography
+        variant='subtitle1'
+        color='text.primary'
+        marginBottom={24}
+      >
+        {selectedNote.body}
+      </Typography>
+    </Stack>
   )
 }
 
-export default NoteDetailWrapper
+NoteDetail.defaultProps = {
+  filteredNoteList: [],
+}
+
+NoteDetail.propTypes = {
+  filteredNoteList: PropTypes.array.isRequired,
+}
+
+export default NoteDetail
