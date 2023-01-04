630aeed339f914aa250c1f146807bc75623d480f
jaballogian <jaballogianrma77@gmail.com>
2023-01-04 22:47:51 +0700

    add a Tooltip component and its children and add on fab click handler function

--- react/2-react-fundamental-1/src/pages/Main.jsx
+++ react/2-react-fundamental-1/src/pages/Main.jsx
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types'
+import { useNavigate } from 'react-router-dom'
 
 // COMPONENTS
 import NoteList from 'components/NoteList'
 
 // MUIS
+import Fab from '@mui/material/Fab'
 import Stack from '@mui/material/Stack'
+import Tooltip from '@mui/material/Tooltip'
+
+// MUI ICONS
+import IconAdd from '@mui/icons-material/Add'
 
 const Main = (props) => {
   const { 
@@ -13,8 +19,16 @@ const Main = (props) => {
     onArchiveNote,
   } = props
 
+  const navigate = useNavigate()
+
+  const onFabClickHandler = (event) => {
+    event.preventDefault()
+    navigate('/add-new')
+  }
+
   return (
     <>
+      {/* CONTENT */}
       <Stack direction='row'>
         {/* ACTIVE NOTES */}
         <NoteList 
@@ -32,6 +46,25 @@ const Main = (props) => {
           onArchiveButtonClick={onArchiveNote}
         />
       </Stack>
+
+      {/* ADD BUTTON */}
+      <Tooltip
+        title='Add a new note' 
+        placement='top'
+      >
+        <Fab
+          href='/add-new'
+          color='primary'
+          onClick={onFabClickHandler}
+          sx={{
+            position: 'fixed',
+            bottom: 40,
+            right: 40,
+          }}
+        >
+          <IconAdd/>
+        </Fab>
+      </Tooltip>
     </>
   )
 }
