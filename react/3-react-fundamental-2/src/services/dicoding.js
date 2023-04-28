2356d0ecc24bd0bcd183b8bc2d3d9119258d250e
jaballogian <jaballogianrma77@gmail.com>
2023-04-28 20:48:02 +0700

    add dicoding service

---
+++ react/3-react-fundamental-2/src/services/dicoding.js
@@ -0,0 +1,169 @@
+// UTILITIES
+import { readAccessTokenFromLocalStorage } from 'utilities/localStorage'
+
+const BASE_URL = 'https://notes-api.dicoding.dev/v1'
+
+async function fetchWithToken(url, options = {}) {
+  return fetch(url, {
+    ...options,
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${readAccessTokenFromLocalStorage()}`,
+    },
+  })
+}
+
+async function login({ email, password }) {
+  const response = await fetch(`${BASE_URL}/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  })
+
+  const responseJson = await response.json()
+
+  if (responseJson.status !== 'success') {
+    alert(responseJson.message)
+    return { error: true, data: null }
+  }
+
+  return { error: false, data: responseJson.data }
+}
+
+async function register({ name, email, password }) {
+  const response = await fetch(`${BASE_URL}/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name, email, password }),
+  })
+
+  const responseJson = await response.json()
+
+  if (responseJson.status !== 'success') {
+    // alert(responseJson.message)
+    return { error: true, message: responseJson.message }
+  }
+
+  return { error: false, message: responseJson.message }
+}
+
+async function getUserLogged() {
+  const response = await fetchWithToken(`${BASE_URL}/users/me`)
+  const responseJson = await response.json()
+
+  if (responseJson.status !== 'success') {
+    return { error: true, data: null }
+  }
+
+  return { error: false, data: responseJson.data }
+}
+
+async function addNote({ title, body }) {
+  const response = await fetchWithToken(`${BASE_URL}/notes`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ title, body }),
+  })
+
+  const responseJson = await response.json()
+
+  if (responseJson.status !== 'success') {
+    return { error: true, data: null }
+  }
+
+  return { error: false, data: responseJson.data }
+}
+
+async function getActiveNotes() {
+  const response = await fetchWithToken(`${BASE_URL}/notes`)
+  const responseJson = await response.json()
+
+  if (responseJson.status !== 'success') {
+    return { error: true, data: null }
+  }
+
+  return { error: false, data: responseJson.data }
+}
+
+async function getArchivedNotes() {
+  const response = await fetchWithToken(`${BASE_URL}/notes/archived`)
+  const responseJson = await response.json()
+
+  if (responseJson.status !== 'success') {
+    return { error: true, data: null }
+  }
+
+  return { error: false, data: responseJson.data }
+}
+
+async function getNote(id) {
+  const response = await fetchWithToken(`${BASE_URL}/notes/${id}`)
+  const responseJson = await response.json()
+
+  if (responseJson.status !== 'success') {
+    return { error: true, data: null }
+  }
+
+  return { error: false, data: responseJson.data }
+}
+
+async function archiveNote(id) {
+  const response = await fetchWithToken(`${BASE_URL}/notes/${id}/archive`, {
+    method: 'POST',
+  })
+
+  const responseJson = await response.json()
+
+  if (responseJson.status !== 'success') {
+    return { error: true, data: null }
+  }
+
+  return { error: false, data: responseJson.data }
+}
+
+async function unarchiveNote(id) {
+  const response = await fetchWithToken(`${BASE_URL}/notes/${id}/unarchive`, {
+    method: 'POST',
+  })
+
+  const responseJson = await response.json()
+
+  if (responseJson.status !== 'success') {
+    return { error: true, data: null }
+  }
+
+  return { error: false, data: responseJson.data }
+}
+
+async function deleteNote(id) {
+  const response = await fetchWithToken(`${BASE_URL}/notes/${id}`, {
+    method: 'DELETE',
+  })
+
+  const responseJson = await response.json()
+
+  if (responseJson.status !== 'success') {
+    return { error: true, data: null }
+  }
+
+  return { error: false, data: responseJson.data }
+}
+
+export {
+  login,
+  register,
+  getUserLogged,
+  addNote,
+  getActiveNotes,
+  getArchivedNotes,
+  getNote,
+  archiveNote,
+  unarchiveNote,
+  deleteNote,
+}
