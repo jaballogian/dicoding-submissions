f68218f8fa09d8bdf612ff8db3b5fc4b6be080dd
jaballogian <jaballogianrma77@gmail.com>
2023-05-03 20:30:34 +0700

    separate the palette values from the theme constant into the colors constant

--- react/3-react-fundamental-2/src/constants/theme.js
+++ react/3-react-fundamental-2/src/constants/theme.js
@@ -1,21 +1,7 @@
 // MUIS
 import { createTheme } from '@mui/material'
-import { alpha } from '@mui/material/styles'
-
-const white = '#FFFFFF'
 
 let customTheme = createTheme({
-  palette: {
-    divider: alpha(white, 0.12),
-    background: {
-      default: '#282a2e', // BLACK
-      paper: '#383f4d', // LIGHT BLACK
-    },
-    text: {
-      primary: white, // WHITE
-      secondary: alpha(white, 0.6), // GREY
-    },
-  },
   spacing: 1,
   typography: {
     fontFamily: [ 'Montserrat', 'sans-serif' ].join(','),
