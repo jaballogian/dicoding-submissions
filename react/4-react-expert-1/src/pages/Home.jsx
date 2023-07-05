a40b848291dcc25687eebaccbcb5972711d3cf70
jaballogian <jaballogianrma77@gmail.com>
2023-07-05 23:07:44 +0700

    add root Stack and thread list component Stack components

--- react/4-react-expert-1/src/pages/Home.jsx
+++ react/4-react-expert-1/src/pages/Home.jsx
@@ -1,9 +1,46 @@
 /* eslint linebreak-style: ["error", "windows"] */
 import React from 'react';
 
+// MUIS
+import Stack from '@mui/material/Stack';
+
+// COMPONENTS
+import ThreadItem from '../components/ThreadItem';
+
+// CONSTANTS
+import {
+  dummyThreadList,
+  dummyUserList,
+} from '../constants/mockAPI';
+
 function Home() {
+  const combinedThreadAndUserLists = dummyThreadList.map((thread) => ({
+    ...thread,
+    user: dummyUserList.find((user) => thread.ownerId === user.id),
+  }));
+
   return (
-    <div>Home</div>
+    <Stack
+      width="100%"
+      maxWidth={1200}
+      alignSelf="center"
+    >
+      {/* THREAD LIST */}
+      <Stack spacing={24}>
+        {combinedThreadAndUserLists.map((thread) => (
+          <ThreadItem
+            key={thread.id}
+            id={thread.id}
+            body={thread.body}
+            createdTime={thread.createdAt}
+            title={thread.title}
+            totalComments={thread.totalComments}
+            name={thread.user.name}
+            avatar={thread.user.avatar}
+          />
+        ))}
+      </Stack>
+    </Stack>
   );
 }
 
