6899c140d81fb89701ef898f8057930aea370e3f
jaballogian <jaballogianrma77@gmail.com>
2023-07-05 23:49:47 +0700

    add title and empty list Typography components

--- react/4-react-expert-1/src/pages/Home.jsx
+++ react/4-react-expert-1/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 // MUIS
 import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
 // COMPONENTS
 import ThreadItem from '../components/ThreadItem';
@@ -25,21 +26,38 @@ function Home() {
       maxWidth={1200}
       alignSelf="center"
     >
-      {/* THREAD LIST */}
-      <Stack spacing={24}>
-        {combinedThreadAndUserLists.map((thread) => (
-          <ThreadItem
-            key={thread.id}
-            id={thread.id}
-            body={thread.body}
-            createdTime={thread.createdAt}
-            title={thread.title}
-            totalComments={thread.totalComments}
-            name={thread.user.name}
-            avatar={thread.user.avatar}
-          />
-        ))}
-      </Stack>
+      {/* TITLE */}
+      <Typography
+        variant="h6"
+        fontWeight={600}
+        color="text.primary"
+        marginBottom={24}
+      >
+        Threads
+      </Typography>
+
+      {combinedThreadAndUserLists.length === 0 ? (
+        // EMPTY LIST TEXT
+        <Typography color="text.primary">
+          No threads available
+        </Typography>
+      ) : (
+        // THREAD LIST
+        <Stack spacing={24}>
+          {combinedThreadAndUserLists.map((thread) => (
+            <ThreadItem
+              key={thread.id}
+              id={thread.id}
+              body={thread.body}
+              createdTime={thread.createdAt}
+              title={thread.title}
+              totalComments={thread.totalComments}
+              name={thread.user.name}
+              avatar={thread.user.avatar}
+            />
+          ))}
+        </Stack>
+      )}
     </Stack>
   );
 }
