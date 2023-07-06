3d95dd89d844f81471db00d60ba2587f1618daa3
jaballogian <jaballogianrma77@gmail.com>
2023-07-06 20:55:28 +0700

    add comment form Stack component

--- react/4-react-expert-1/src/pages/ThreadDetail.jsx
+++ react/4-react-expert-1/src/pages/ThreadDetail.jsx
@@ -8,9 +8,14 @@ import moment from 'moment';
 // MUIS
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
 // CONSTANTS
@@ -19,6 +24,9 @@ import {
   dummyUserList,
 } from '../constants/mockAPI';
 
+// HOOKS
+import useInput from '../hooks/useInput';
+
 function ThreadDetail() {
   const combinedThreadAndUserLists = dummyThreadList.map((thread) => ({
     ...thread,
@@ -26,8 +34,15 @@ function ThreadDetail() {
   }));
   const threadDetail = combinedThreadAndUserLists[2];
 
+  const [inputComment, setInputComment] = useInput('');
+
   return (
-    <Card>
+    <Card sx={{
+      maxWidth: 1200,
+      alignSelf: 'center',
+      width: '100%',
+    }}
+    >
       {/* HEADER */}
       <CardHeader
         avatar={(<Avatar src={threadDetail.user.avatar} />)}
@@ -39,14 +54,51 @@ function ThreadDetail() {
       <CardContent>
         {/* TITLE */}
         <Typography
+          variant="h6"
           fontWeight={600}
-          marginBottom={8}
+          marginBottom={20}
         >
           {threadDetail.title}
         </Typography>
 
         {/* BODY */}
         <Box dangerouslySetInnerHTML={{ __html: threadDetail.body }} />
+
+        {/* COMMENT FORM */}
+        <Stack
+          marginTop={40}
+          spacing={16}
+          width="100%"
+          component="form"
+          // onSubmit={submitFormHandler}
+        >
+          {/* COMMENT INPUT */}
+          <FormControl fullWidth>
+            <InputLabel>
+              Comment
+            </InputLabel>
+
+            <OutlinedInput
+              autoFocus
+              placeholder="Write your comment here"
+              type="text"
+              name="comment"
+              label="Comment"
+              multiline
+              rows={4}
+              value={inputComment}
+              onChange={setInputComment}
+            />
+          </FormControl>
+
+          {/* COMMENT BUTTON */}
+          <Button
+            variant="contained"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </Stack>
       </CardContent>
     </Card>
   );
