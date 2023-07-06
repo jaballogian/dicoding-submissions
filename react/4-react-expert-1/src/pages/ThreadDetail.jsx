a5a3c536af3169dfc14dc536499913cc8519c3ea
jaballogian <jaballogianrma77@gmail.com>
2023-07-06 20:37:20 +0700

    add root Card, Card Header, and Card Content components

--- react/4-react-expert-1/src/pages/ThreadDetail.jsx
+++ react/4-react-expert-1/src/pages/ThreadDetail.jsx
@@ -1,9 +1,54 @@
 /* eslint linebreak-style: ["error", "windows"] */
 import React from 'react';
 
+// DATE AND TIME
+// eslint-disable-next-line import/no-extraneous-dependencies
+import moment from 'moment';
+
+// MUIS
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Typography from '@mui/material/Typography';
+
+// CONSTANTS
+import {
+  dummyThreadList,
+  dummyUserList,
+} from '../constants/mockAPI';
+
 function ThreadDetail() {
+  const combinedThreadAndUserLists = dummyThreadList.map((thread) => ({
+    ...thread,
+    user: dummyUserList.find((user) => thread.ownerId === user.id),
+  }));
+  const threadDetail = combinedThreadAndUserLists[2];
+
   return (
-    <div>ThreadDetail</div>
+    <Card>
+      {/* HEADER */}
+      <CardHeader
+        avatar={(<Avatar src={threadDetail.user.avatar} />)}
+        title={threadDetail.user.name}
+        subheader={moment(threadDetail.createdAt).fromNow()}
+      />
+
+      {/* CONTENT */}
+      <CardContent>
+        {/* TITLE */}
+        <Typography
+          fontWeight={600}
+          marginBottom={8}
+        >
+          {threadDetail.title}
+        </Typography>
+
+        {/* BODY */}
+        <Box dangerouslySetInnerHTML={{ __html: threadDetail.body }} />
+      </CardContent>
+    </Card>
   );
 }
 
