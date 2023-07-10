eb30e1ecc98c4d8e7d73fd2eeff56234e945bc14
jaballogian <jaballogianrma77@gmail.com>
2023-07-10 23:20:39 +0700

    add api utility

---
+++ react/4-react-expert-1/src/utilities/api.js
@@ -0,0 +1,28 @@
+/* eslint linebreak-style: ["error", "windows"] */
+const api = (() => {
+  const BASE_URL = 'https://forum-api.dicoding.dev/v1';
+
+  function putAccessToken(token) {
+    localStorage.setItem('accessToken', token);
+  }
+
+  function getAccessToken() {
+    return localStorage.getItem('accessToken');
+  }
+
+  async function fetchWithAuth(url, options = {}) {
+    return fetch(url, {
+      ...options,
+      headers: {
+        ...options.headers,
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
+    });
+  }
+
+  return {
+    putAccessToken,
+    getAccessToken,
+  };
+})();
+export default api;
