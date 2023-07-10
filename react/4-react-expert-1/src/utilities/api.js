92eb34271ee6d5acd21721604d570c334a2256d4
jaballogian <jaballogianrma77@gmail.com>
2023-07-10 23:49:23 +0700

    add get all users function

--- react/4-react-expert-1/src/utilities/api.js
+++ react/4-react-expert-1/src/utilities/api.js
@@ -102,6 +102,22 @@ const api = (() => {
     return threads;
   }
 
+  async function getAllUsers() {
+    const response = await fetch(`${BASE_URL}/users`);
+
+    const responseJson = await response.json();
+
+    const { status, message } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    const { data: { users } } = responseJson;
+
+    return users;
+  }
+
   return {
     putAccessToken,
     getAccessToken,
@@ -109,6 +125,7 @@ const api = (() => {
     loginUser,
     getOwnProfile,
     getAllThreads,
+    getAllUsers,
   };
 })();
 export default api;
