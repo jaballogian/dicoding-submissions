79482041ffc4b0398f3bf3a401307aec056f6256
jaballogian <jaballogianrma77@gmail.com>
2023-07-12 20:50:30 +0700

    add action creators and thunk functions for the threads state

---
+++ react/4-react-expert-1/src/states/threads/action.js
@@ -0,0 +1,44 @@
+/* eslint linebreak-style: ["error", "windows"] */
+import api from '../../utilities/api';
+
+const ActionType = {
+  RECEIVE_THREADS: 'RECEIVE_THREADS',
+  ADD_THREAD: 'ADD_THREAD',
+};
+
+function receiveThreadsActionCreator(threads) {
+  return {
+    type: ActionType.RECEIVE_THREADS,
+    payload: {
+      threads,
+    },
+  };
+}
+
+function addThreadActionCreator(thread) {
+  return {
+    type: ActionType.ADD_THREAD,
+    payload: {
+      thread,
+    },
+  };
+}
+
+// THUNK FUNCTION FOR CREATING THREAD
+function asyncAddThread({ title, body, category = '' }) {
+  return async (dispatch) => {
+    try {
+      const thread = await api.createThread({ title, body, category });
+      dispatch(addThreadActionCreator(thread));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+export {
+  ActionType,
+  receiveThreadsActionCreator,
+  addThreadActionCreator,
+  asyncAddThread,
+};
