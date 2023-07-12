f415ecc0f88bdf8765e6909247363d49dab8428b
jaballogian <jaballogianrma77@gmail.com>
2023-07-12 21:04:56 +0700

    add a thunk function for the shared state

---
+++ react/4-react-expert-1/src/states/shared/action.js
@@ -0,0 +1,21 @@
+/* eslint linebreak-style: ["error", "windows"] */
+import api from '../../utilities/api';
+import { receiveThreadsActionCreator } from '../threads/action';
+import { receiveUsersActionCreator } from '../users/action';
+
+// THUNK FUNCTION FOR RECEIVING THE USERS AND THREADS
+function asyncPopulateUsersAndThreads() {
+  return async (dispatch) => {
+    try {
+      const users = await api.getAllUsers();
+      const threads = await api.getAllThreads();
+
+      dispatch(receiveUsersActionCreator(users));
+      dispatch(receiveThreadsActionCreator(threads));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+export default asyncPopulateUsersAndThreads;
