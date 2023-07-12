ded6f4284f35885c732f4ea36f7c89fbbd5a7464
jaballogian <jaballogianrma77@gmail.com>
2023-07-12 21:12:22 +0700

    wrap the root component with the Provider component

--- react/4-react-expert-1/src/index.jsx
+++ react/4-react-expert-1/src/index.jsx
@@ -10,24 +10,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+// REDUX
+import { Provider } from 'react-redux';
+
 // CONSTANTS
 import customTheme from './constants/theme';
 
 // COMPONENTS
 import App from './App';
 
+// STATES
+import store from './states';
+
 // STYLES
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={customTheme}>
-      <StyledEngineProvider injectFirst>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </StyledEngineProvider>
-    </ThemeProvider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <ThemeProvider theme={customTheme}>
+        <StyledEngineProvider injectFirst>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </StyledEngineProvider>
+      </ThemeProvider>
+    </React.StrictMode>
+  </Provider>,
 );
