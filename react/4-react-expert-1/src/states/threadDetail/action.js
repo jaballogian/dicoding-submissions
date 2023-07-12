6a104f395e45a2be085806786d4b65c769fb9a65
jaballogian <jaballogianrma77@gmail.com>
2023-07-12 20:58:43 +0700

    add action creators and thunk functions for the thread detail state

---
+++ react/4-react-expert-1/src/states/threadDetail/action.js
@@ -0,0 +1,42 @@
+/* eslint linebreak-style: ["error", "windows"] */
+import api from '../../utilities/api';
+
+const ActionType = {
+  RECEIVE_THREAD_DETAIL: 'RECEIVE_THREAD_DETAIL',
+  CLEAR_THREAD_DETAIL: 'CLEAR_THREAD_DETAIL',
+};
+
+function receiveThreadDetailActionCreator(threadDetail) {
+  return {
+    type: ActionType.RECEIVE_THREAD_DETAIL,
+    payload: {
+      threadDetail,
+    },
+  };
+}
+
+function clearThreadDetailActionCreator() {
+  return {
+    type: ActionType.CLEAR_THREAD_DETAIL,
+  };
+}
+
+// THUNK FUNCTION FOR RECEIVING THREAD DETAIL
+function asyncReceiveThreadDetail(threadId) {
+  return async (dispatch) => {
+    dispatch(clearThreadDetailActionCreator());
+    try {
+      const threadDetail = await api.getThreadDetail(threadId);
+      dispatch(receiveThreadDetailActionCreator(threadDetail));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+export {
+  ActionType,
+  receiveThreadDetailActionCreator,
+  clearThreadDetailActionCreator,
+  asyncReceiveThreadDetail,
+};
